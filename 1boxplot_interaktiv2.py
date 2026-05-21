72bf2e38cf262f2e3bc609e7bf83c813791005e1
szauterbenedek <szauter.benedek@gmail.com>
2026-05-21 12:08:14 +0200

    Update 1boxplot_interaktiv2.py
    debug

--- 1boxplot_interaktiv2.py
+++ 1boxplot_interaktiv2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as str  # Behozzuk a Streamlitet (st néven)
+import streamlit as st
 import io
 
 # =========================
